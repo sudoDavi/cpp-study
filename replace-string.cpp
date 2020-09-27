a641caa46cef35c2bf78c954b020eca8b2476726
Davi Wendt <daviwendt@gmail.com>
2020-09-27 10:28:47 -0300

    chapter S.4.4c learncpp

---
+++ replace-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string str{ "I saw a red car yesterday." };
+
+	str.replace(8, 3, "blue");
+
+	std::cout << str << '\n';
+
+	return 0;
+}
