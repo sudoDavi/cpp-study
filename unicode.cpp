cbe2da7e64fb5b97478858eca37cbb09e7b7f729
Davi Wendt <daviwendt@gmail.com>
2020-10-14 10:25:51 -0300

    modified unicode test

--- unicode.cpp
+++ unicode.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main() {
 	std::cout << "Unicode test: " << "\u0414" << '\n';
+	// Test for unicode input
+	std::cout << "Input a unicode character like \u0414, with the input method you prefer: ";
+	std::string unicodeInput;
+	std::cin >> unicodeInput;
+	std::cout << "You entered " << unicodeInput << '\n';
 
 	return 0;
 }
