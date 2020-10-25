d6462f528751e488be883464137f4f77b8b3a620
Davi Wendt <daviwendt@gmail.com>
2020-10-25 15:45:50 -0300

    chapter P.6.12a learncpp

---
+++ easy-array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string_view>
+#include <string>
+
+int main() {
+	const std::string_view names[]{ "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly" };
+
+	std::cout << "Enter a name: ";
+	std::string findName;
+	std::getline(std::cin, findName);
+
+	// for each loop example
+	for (auto name : names) {
+		if (name == findName) {
+			std::cout << findName << " was found.\n";
+			return 0;
+		}
+	}
+
+	std::cout << findName << " was not found.\n";
+
+	return 0;
+}
