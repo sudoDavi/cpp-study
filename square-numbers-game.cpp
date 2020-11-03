fb95118f18a5e959c22cbfa138bb5e91cc5fe4a0
Davi Wendt <daviwendt@gmail.com>
2020-11-02 21:22:34 -0300

    chapter F.7.16 learncpp
    Square numbers game like the book

--- square-numbers-game.cpp
+++ square-numbers-game.cpp
@@ -3,6 +3,7 @@
 #include <ctime> // for std::time
 #include <vector>
 #include <algorithm> // for std::find
+#include <cmath> // for std::abs
 
 namespace Random {
 	// Initialize mersenne twister based on clock
@@ -14,54 +15,80 @@ namespace Random {
 	}
 }
 
-void guessNumbers(std::vector<int>& list) {
-	while (true) {
-		int number{};
-		std::cin >> number;
+namespace Config {
+	constexpr int RandomMin{ 2 };
+	constexpr int RandomMax{ 4 };
+}
 
-		auto foundNumber{ std::find(list.begin(), list.end(), number) };
-	
-		if (foundNumber != list.end()) {
-			list.erase(foundNumber);
-			if (list.empty()) {
+using list_type = std::vector<int>;
+
+list_type generateNumbers(int multiplier) {
+	std::cout << "Start where? ";
+	int start{};
+	std::cin >> start;
+
+	std::cout << "How many? ";
+	int howMany{};
+	std::cin >> howMany;
+
+	list_type numbers;
+	// Fill the list with numbers which follow the formula number^2 * multiplier
+	// The list is as long as howMany, we just offset it by start
+	for (int number{ start }; number < start + howMany; ++number) {
+		numbers.push_back((number * number) * multiplier);
+	}
+
+	return numbers;
+}
+
+void playGame(list_type& numbers, int multiplier) {
+	std::cout << "I generated " << numbers.size() << " square numbers. Do you know what each number is after multiplying it by " << multiplier << "?\n";
+	// While not loosing or winning get the guessed number from the user
+	while (true) {
+		int guessed{};
+		std::cin >> guessed;
+
+		// Search the guessed number in the numbers array
+		auto searched{ std::find(numbers.begin(), numbers.end(), guessed) };
+
+		// std::find returns the .end() of the array if it didn't find the searched parameter
+		if (searched != numbers.end()) { // If it enters here it means we guessed the number correctly
+			// Remove the guessed number from the array
+			numbers.erase(searched);
+
+			// If after the erase(), numbers is empty, we've found all numbers and can stop the program
+			if (numbers.empty()) {
 				std::cout << "Nice! You found all numbers, good job!\n";
 				break;
 			}
-			std::cout << "Nice! " << list.size() << " number(s) left.\n";
+			else { // If not, we still have numbers to guess
+				std::cout << "Nice! " << numbers.size() << " number(s) left.\n";
+			}
 		}
-		else {
-			std::cout << number << " is wrong! Try " << list.front() << " next time.\n";
+		else { // We guessed the wrong number
+			// Find the closest number in the array to the guessed one
+			auto closest{ std::min_element(
+				numbers.begin(),
+				numbers.end(),
+				// Lambda declaration to find the closest number
+				[guessed](const auto& a, const auto& b) {
+					return std::abs(a - guessed) < std::abs(b - guessed);
+				})
+			};
+
+			// Break the loop because we lost the game
+			std::cout << guessed << " is wrong! Try " << *closest << " next time.\n";
 			break;
 		}
 	}
 }
 
 int main() {
-	std::cout << "Start where? ";
-	int startSqrt{};
-	std::cin >> startSqrt;
-
-	std::cout << "How many? ";
-	int howMany{};
-	std::cin >> howMany;
-	
-	int randomMult{ Random::Get(2, 4) };
-
-	auto generateList { [randomMult](int start, int count) -> std::vector<int> {
-		std::vector<int> list;
-
-		for (int number{ start }; number < start + count; ++number) {
-			int squareNumber{ number * number };
-			list.push_back(squareNumber * randomMult);
-		}
-		return list;
-	} };
-
-	std::vector<int> list{ generateList(startSqrt, howMany) };
-
-	std::cout << "I generated " << list.size() << " square numbers. ";
-	std::cout << "Do you know what each number is after multiplying it by " << randomMult << "?\n";
-	guessNumbers(list);
+	int randomMult{ Random::Get(Config::RandomMin, Config::RandomMax) };
+	// Generate the numbers using the random integer
+	list_type numbers{ generateNumbers(randomMult) };
+	// Play the game with the generated numbers and the random number
+	playGame(numbers, randomMult);
 
 	return 0;
 }
