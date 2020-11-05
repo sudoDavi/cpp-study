bf4c76afe9b287add30a19972e7b96c236f1f9c4
Davi Wendt <daviwendt@gmail.com>
2020-11-05 10:02:26 -0300

    chapter 8.3 learncpp
    Public vs private access modifiers
    Question 2.b

--- Point3d.cpp
+++ Point3d.cpp
@@ -18,17 +18,30 @@ public:
 		std::cout << printable << '\n';
 	}
 
-	// bool isEqual(const Point3d& otherP) {
-		// return (m_x == otherP.m_x && m_y == otherP.m_y && m_z == otherP.m_z);
-	// }
+	bool isEqual(const Point3d& otherP) {
+		return (m_x == otherP.m_x && m_y == otherP.m_y && m_z == otherP.m_z);
+	}
 };
 
 int main() {
-	Point3d point;
-	point.setValues(1, 2, 3);
+	Point3d point1;
+	point1.setValues(1, 2, 3);
 
-	point.print();
-	std::cout << '\n';
+	Point3d point2;
+	point2.setValues(1, 2, 3);
+
+	if (point1.isEqual(point2))
+		std::cout << "point1 and point2 are equal\n";
+	else
+		std::cout << "point1 and point2 are different\n";
+
+	Point3d point3;
+	point3.setValues(4, 2, 0);
+
+	if (point1.isEqual(point3))
+		std::cout << "point1 and point3 are equal\n";
+	else
+		std::cout << "point1 and point3 are different\n";
 
 	return 0;
 }
