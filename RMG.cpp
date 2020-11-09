ef5b583b15877821f5c17aaf2a7ab2dd1d250696
Davi Wendt <daviwendt@gmail.com>
2020-11-09 16:57:15 -0300

    chapter 8.x learncpp
    Chapter 8 comprehensive quiz
    Question 3.c and 3.d

--- RMG.cpp
+++ RMG.cpp
@@ -1,27 +1,38 @@
 // RMG - Random Monster Generator
 #include <string>
 
-enum class MonsterType {
-	DRAGON,
-	GOBLIN,
-	OGRE,
-	ORC,
-	SKELETON,
-	TROLL,
-	VAMPIRE,
-	ZOMBIE,
-
-	MAX_MONSTER_TYPES
-};
-
 class Monster {
+// Nested types
+public:
+	enum class Type {
+		DRAGON,
+		GOBLIN,
+		OGRE,
+		ORC,
+		SKELETON,
+		TROLL,
+		VAMPIRE,
+		ZOMBIE,
+
+		MAX_MONSTER_TYPES
+	};
+
+// Member variables
 private:
-	MonsterType m_type{};
+	Type m_type{};
 	std::string m_name{};
 	std::string m_roar{};
 	int m_hp{};
+
+// Member functions
 public:
 	Monster(
-		MonsterType type = MonsterType::ZOMBIE, const std::string& name = "Zombie", const std::string& roar = "EEEEEEERR", int hp = 10
+		Type type = Type::ZOMBIE, const std::string& name = "Zombie", const std::string& roar = "EEEEEEERR", int hp = 10
 	) : m_type{type}, m_name{name}, m_roar{roar}, m_hp{hp} {}
 };
+
+int main() {
+	Monster skeleton{ Monster::Type::SKELETON, "Bones", "*rattle*", 4 };
+
+	return 0;
+}
