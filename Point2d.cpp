99041df034d30c0edb51d9921766c0dcbe518813
Davi Wendt <daviwendt@gmail.com>
2020-11-09 16:32:19 -0300

    chapter 8.x learncpp
    Chapter 8 comprehensive quiz
    Question 1.c

--- Point2d.cpp
+++ Point2d.cpp
@@ -12,17 +12,19 @@ public:
 		std::cout << "Point2d(" << m_x << ", " << m_y << ")\n";
 	}
 
-	double distanceTo(const Point2d& point) const {
-		return std::sqrt((m_x - point.m_x) * (m_x - point.m_x) + (m_y - point.m_y) * (m_y - point.m_y));
-	}
+	friend double distanceFrom(const Point2d& point1, const Point2d& point2);
 };
+
+double distanceFrom(const Point2d& point1, const Point2d& point2) {
+	return std::sqrt((point1.m_x - point2.m_x) * (point1.m_x - point2.m_x) + (point1.m_y - point2.m_y) * (point1.m_y - point2.m_y));
+}
 
 int main() {
 	Point2d first{};
 	Point2d second{ 3.0, 4.0 };
 	first.print();
 	second.print();
-	std::cout << "Distance between two points: " << first.distanceTo(second) << "\n";
+	std::cout << "Distance between two points: " << distanceFrom(first, second) << "\n";
 
 	return 0;
 }
