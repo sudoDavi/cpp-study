e7c636cde4ca03e37c928f64d672cc16f9dbacdd
Davi Wendt <daviwendt@gmail.com>
2020-11-09 21:59:03 -0300

    chapter 8.x learncpp
    Chapter 8 comprehensive quiz
    Question 3.g and 3.h

--- RMG.cpp
+++ RMG.cpp
@@ -4,6 +4,8 @@
 #include <array>
 #include <iostream>
 #include <cstddef> // for std::size_t
+#include <ctime> // for std::time()
+#include <cstdlib> // for std::rand() and srand()
 
 class Monster {
 // Nested types
@@ -44,14 +46,32 @@ public:
 	}
 };
 
+// Assumes srand() has already been called
+// Gets a random number from min inclusive up to max inclusive
+int getRandomNumber(int min, int max) {
+	static constexpr double fraction{ 1.0 / (static_cast<double>(RAND_MAX) + 1.0) }; // Is static so we only calculate this value once
+	return static_cast<int>(std::rand() * fraction * (max - min + 1) + min);
+}
+
 class MonsterGenerator {
 public:
 	static Monster generateMonster() {
-		return Monster{ Monster::Type::SKELETON, "Bones", "*rattle*", 4 };
+		static constexpr std::array s_names{ "Mortimer", "Guli", "Uptyr", "Bones", "Priot", "Vladimir" };
+		static constexpr std::array s_roars{ "*peep*", "*gulp*", "*rattles*", "*pop*", "*quiqui*", "*squeak*" };
+
+		return Monster{ 
+			static_cast<Monster::Type>(getRandomNumber(0, static_cast<int>(Monster::Type::MAX_MONSTER_TYPES) - 1)),
+			s_names[static_cast<std::size_t>(getRandomNumber(0 , 5))],
+			s_roars[static_cast<std::size_t>(getRandomNumber(0 , 5))],
+			getRandomNumber(1, 100)
+		};
 	}
 };
 
 int main() {
+	std::srand(static_cast<unsigned int>(std::time(nullptr))); // Seed the RNG with time
+	std::rand(); // For compatibility with VSC we're discarding the first number generated
+
 	Monster m{ MonsterGenerator::generateMonster() };
 	m.print();
 
