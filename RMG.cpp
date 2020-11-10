f0c7f6a79908cdf0f59e1a920e05400852fcfca5
Davi Wendt <daviwendt@gmail.com>
2020-11-09 21:41:37 -0300

    chapter 8.x learncpp
    Chapter 8 comprehensive quiz
    Question 3.e and 3.f

--- RMG.cpp
+++ RMG.cpp
@@ -1,5 +1,9 @@
 // RMG - Random Monster Generator
 #include <string>
+#include <string_view>
+#include <array>
+#include <iostream>
+#include <cstddef> // for std::size_t
 
 class Monster {
 // Nested types
@@ -29,10 +33,27 @@ public:
 	Monster(
 		Type type = Type::ZOMBIE, const std::string& name = "Zombie", const std::string& roar = "EEEEEEERR", int hp = 10
 	) : m_type{type}, m_name{name}, m_roar{roar}, m_hp{hp} {}
+
+	std::string_view getTypeString() const {
+		static constexpr std::array typeStrings{ "dragon", "goblin", "ogre", "orc", "skeleton", "troll", "vampire", "zombie" };
+		return typeStrings[static_cast<std::size_t>(m_type)];
+	}
+
+	void print() const {
+		std::cout << m_name << " the " << getTypeString() << " has " << m_hp << " hit points and says " << m_roar << '\n';
+	}
+};
+
+class MonsterGenerator {
+public:
+	static Monster generateMonster() {
+		return Monster{ Monster::Type::SKELETON, "Bones", "*rattle*", 4 };
+	}
 };
 
 int main() {
-	Monster skeleton{ Monster::Type::SKELETON, "Bones", "*rattle*", 4 };
+	Monster m{ MonsterGenerator::generateMonster() };
+	m.print();
 
 	return 0;
 }
